04803c845e4c28d621575d4038884c8fc4f0a764
mehziya0352 <mehziya0352@gmail.com>
2025-10-18 18:48:38 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-springsboot-state-bucket"
+    bucket = "my-springsboot-ste-bucket"
     key    = "secure-devops-pipeline-minimal/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform-locks"
