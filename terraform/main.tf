d97bdcc3e1e7719ee741ce0d8c0c13e45137a086
mehziya0352 <mehziya0352@gmail.com>
2025-10-18 16:43:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -243,15 +243,4 @@ resource "aws_db_instance" "mysql" {
   }
 }
 
-# ----------------------------
-# Outputs
-# ----------------------------
-output "builder_public_ip" {
-  description = "Public IP of the builder instance (SSH here to run Ansible)"
-  value       = aws_instance.builder.public_ip
-}
 
-output "rds_endpoint" {
-  description = "RDS endpoint (connect from app servers inside VPC)"
-  value       = aws_db_instance.mysql.endpoint
-}
