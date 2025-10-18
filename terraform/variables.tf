c157d365abec2aa50fc042f92b46d24b97f6d86a
mehziya0352 <mehziya0352@gmail.com>
2025-10-18 10:37:35 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,11 @@
 variable "region" { default = "us-east-1" }
 variable "instance_type" { default = "t2.micro" }
-variable "key_name" { type = string }
+variable "key_name" { 
+    type = string 
+    default = "mujahed"
+}
 variable "db_user" { default = "admin" }
-variable "db_password" { type = string }
-variable "my_ip" { type = string } # your public IP /32
+variable "db_password" {
+    type = string 
+    default = "adminroot"
+}
