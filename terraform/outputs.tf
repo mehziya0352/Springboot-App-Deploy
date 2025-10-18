a9a747b455fe322094a2c8ca2d894ec423112381
mehziya0352 <mehziya0352@gmail.com>
2025-10-18 16:42:51 +0530

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,12 @@
+# ----------------------------
+# Outputs
+# ----------------------------
+output "ec2_public_ip" {
+  description = "Public IP of the builder instance (SSH here to run Ansible)"
+  value       = aws_instance.builder.public_ip
+}
+
+output "rds_endpoint" {
+  description = "RDS endpoint (connect from app servers inside VPC)"
+  value       = aws_db_instance.mysql.endpoint
+}
