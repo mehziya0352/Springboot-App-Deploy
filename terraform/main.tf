030db59f9f6bdf640a563b943a41cc2f9a3ea772
mehziya0352 <mehziya0352@gmail.com>
2025-10-18 11:50:29 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -140,7 +140,7 @@ resource "aws_security_group" "app_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [local.public_ip_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress { 
     from_port   = 0 
